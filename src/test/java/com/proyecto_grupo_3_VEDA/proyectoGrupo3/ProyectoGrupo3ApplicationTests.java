e83989b40cc1a7ae2f63768db19d44da8a173cc7
Esteban Sanchez <easanrui@gmail.com>
2022-10-28 14:35:20 -0600

    creacion del proyecto - carpetas necesarias

---
+++ src/test/java/com/proyecto_grupo_3_VEDA/proyectoGrupo3/ProyectoGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_grupo_3_VEDA.proyectoGrupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
