a64a06ffb2b8c0dbe229b8df3ac9dd46f29107d7
Esteban Sanchez <easanrui@gmail.com>
2022-11-30 11:39:10 -0600

    Se realizan las validaciones de formularios y se agrega la seleccion de proveedor para los productos

--- src/main/java/com/proyecto_grupo_3_VEDA/proyectoGrupo3/entity/Human.java
+++ src/main/java/com/proyecto_grupo_3_VEDA/proyectoGrupo3/entity/Human.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -29,6 +31,7 @@ public class Human implements Serializable{
     private String permissions = "";
     private String roles = "";
     private int active;
+  
 
     public long getId() {
         return id;
@@ -125,7 +128,6 @@ public class Human implements Serializable{
     public void setActive(int active) {
         this.active = active;
     }
-
     
 //    Metodos para autenticacion y seguridad
             
